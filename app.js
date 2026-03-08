69034a1c190185080e86b2df533f39762fb33287
Mohammad Anwar <anwar.hussen.pro@gmail.com>
2026-03-08 16:18:50 +0600

    show issue details and modal

--- app.js
+++ app.js
@@ -137,5 +137,84 @@ tabContainer.addEventListener('click', (e) => {
     }
 });
 
+async function showIssueModal(id) {
+    try {
+        const res = await fetch(`${API_URL}/issue/${id}`);
+        const result = await res.json();
+        const issue = result.data || result;
 
+
+        const isGreen = issue.status.toLowerCase() === 'open';
+        const borderColor = isGreen ? 'border-[#00A96E]' : 'border-[#A855F7]';
+        const statusBadge = isGreen ? 'bg-[#00A96E] text-white' : 'bg-[#A855F7] text-white';
+       const createDateFormatDate= new Date(issue.createdAt).toLocaleDateString('en-US').trim();
+       const updatedAtDateFormatDate= new Date(issue.updatedAt).toLocaleDateString('en-US')
+
+      
+        let priorityClass = 'bg-gray-100 text-gray-600';
+        const priority = (issue.priority || 'LOW').toUpperCase();
+        if (priority === 'HIGH') priorityClass = 'bg-[#EF4444] ';
+        if (priority === 'MEDIUM') priorityClass = 'bg-[#D97706]';
+        if (priority === 'LOW') priorityClass = 'bg-[#10B981]';
+
+        
+        const container = document.getElementById('modal_container');
+        container.className = `modal-box w-11/12 max-w-2xl p-8 rounded-lg border-t-4 ${borderColor} bg-white shadow-2xl relative`;
+
+    
+        const content = document.getElementById('modal_content');
+        content.innerHTML = `
+            <div class="">
+                <h2 class="font-bold text-2xl text-[#1F2937]">${issue.title}</h2>
+
+                <div class="flex items-center gap-3 mt-4">
+                    <span class="badge px-2 py-1.5 ${statusBadge} font-bold">${issue.status}</span>
+                    <span class="font-normal text-sm text-[#1F2937]">
+                    <span class="w-1 h-1 border border-[#64748B] bg-[#64748B] text-center rounded-full inline-block"></span>
+                    Opened by ${issue.author.trim()||'Unknown'}</span>
+                    <span class="text-xs font-normal text-[#64748B]">
+                    <span class="w-1 h-1 border border-[#64748B] bg-[#64748B] rounded-full inline-block"></span>
+                    ${createDateFormatDate}
+                    </span>
+                </div>
+
+                <div class="my-6">
+                    ${HTMLBadgeLables(issue.labels)}
+                </div>
+
+        
+                <p class="font-normal text-base text-[#64748B] text-wrap my-6">
+                    ${issue.description || 'No description provided.'}
+                </p>
+
+                <div class="grid grid-cols-2 md:grid-cols-4 gap-4 bg-gray-50 p-6 rounded-xl text-center mt-8">
+                    <div>
+                        <p class="tfont-normal text-base text-[#64748B]">Assignee</p>
+                        <p class="font-semibold text-base text-[#1F2937]">${issue.assignee || 'Not assigned'}</p>
+                    </div>
+                    <div>
+                        <p class="font-normal text-base text-[#64748B]">Priority</p>
+                        <p class="font-medium text-white text-xs text-center rounded-full w-[60%] mx-auto py-1.5 px-4 ${priorityClass}">
+                            ${priority}
+                        </p>
+                    </div>
+                    <div>
+                        <p class=" font-normal text-base text-[#64748B]">Author</p>
+                        <p class="font-bold text-xs mt-1">${issue.author || 'Unknown'}</p>
+                    </div>
+                    <div>
+                        <p class=" font-normal text-base text-[#64748B]">updatedAt</p>
+                        <p class="font-bold text-xs mt-1">${updatedAtDateFormatDate}</p>
+                    </div>
+                </div>
+            </div>
+        `;
+
+      
+        document.getElementById('my_modal_1').showModal();
+    } catch (err) {
+        console.error("Modal Data Error:", err);
+        alert("Failed to load issue details");
+    }
+}
 
